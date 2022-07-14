ff8ce2ddbd51244223552cf7685a28ce5010f90b
sohyun in <sohyun7305@naver.com>
2022-07-14 21:05:05 +0900

    파이어스토어

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
 
-import { BrowserRouter } from "react-router-dom";
-
-// import { createStore, applyMiddleware } from 'redux';
+// 우리의 버킷리스트에 리덕스를 주입해줄 프로바이더를 불러옵니다!
 import { Provider } from "react-redux";
+// 연결할 스토어도 가지고 와요.
 import store from "./redux/configStore";
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// import thunMiddleware from 'redux-thunk';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
